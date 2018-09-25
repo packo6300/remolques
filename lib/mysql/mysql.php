6a8077cdd7d3faa754e5f508a6964ebdabbcd6e8
Packo Delgado <packo6300@gmail.com>
2018-09-24 23:34:03 -0600

    db try catch

--- lib/mysql/mysql.php
+++ lib/mysql/mysql.php
@@ -1,7 +1,10 @@
 <?php
- if(!isset($_SESSION)) { 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+if(!isset($_SESSION)) { 
         session_start(); 
-    } 
+} 
 function getConnection(){
     $host="localhost";
     $dbu="root";
